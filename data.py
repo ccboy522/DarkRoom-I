09a3bf4ec12bba22921a3e472bb8a24101ff74bd
Tony 张 <55337546+ccboy522@users.noreply.github.com>
2020-01-06 17:50:33 +0800

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+ways = ["You have nothing to eat!",
+        "Your joke is less funny than your father's!",
+        "BOOOOOOOOOOOOOOOOOM",
+        "You died because you are too poor.",
+        "Your Mum kill you because you haven't do your homework yet."
+        "You died of fear."
+        "You died because you didn't do your homework."
+        "You're dead. There's no reason"
+        "Virus invasion!"
+        "Reject a face-saving offer."
+        "Get ready for your death!"]

f7d1f5fca4b9bbc16b8ddb69e7c32be3a8050ef9
Tony 张 <55337546+ccboy522@users.noreply.github.com>
2020-01-12 10:57:09 +0800

    Create data.h

---
+++ data.h
@@ -0,0 +1,19 @@
+# include<iostream>
+# include<string>
+# include<ctime>
+using namespace std;
+void print(){
+	srand(int(time(0)));
+	string data[11] = {"You have nothing to eat!",
+    	               "Your joke is less funny than your father's!",
+    	               "BOOOOOOOOOOOOOOOOOM",
+    	               "You died because you are too poor.",
+					           "Your Mum kill you because you haven't do your homework yet.",
+					           "You died of fear.",
+				             "You died because you didn't do your homework.",
+                     "You're dead. There's no reason",
+                     "Virus invasion!",
+                     "Reject a face-saving offer.",
+                     "Get ready for your death!"};
+	cout << data[rand()%11];
+}

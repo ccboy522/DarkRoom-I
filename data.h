0ea54c18a0730fb30a98529dc7037905ec3557b3
Tony 张 <55337546+ccboy522@users.noreply.github.com>
2020-01-12 10:58:43 +0800

    Update data.h

--- data.h
+++ data.h
@@ -8,12 +8,12 @@ void print(){
     	               "Your joke is less funny than your father's!",
     	               "BOOOOOOOOOOOOOOOOOM",
     	               "You died because you are too poor.",
-					           "Your Mum kill you because you haven't do your homework yet.",
-					           "You died of fear.",
-				             "You died because you didn't do your homework.",
-                     "You're dead. There's no reason",
-                     "Virus invasion!",
-                     "Reject a face-saving offer.",
-                     "Get ready for your death!"};
+		       "Your Mum kill you because you haven't do your homework yet.",
+		       "You died of fear.",
+		       "You died because you didn't do your homework.",
+                       "You're dead. There's no reason",
+                       "Virus invasion!",
+                       "Reject a face-saving offer.",
+                       "Get ready for your death!"};
 	cout << data[rand()%11];
 }

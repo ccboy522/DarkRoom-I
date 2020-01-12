9990fc8a1da77f710c45492d7601d237beb828ae
Tony 张 <55337546+ccboy522@users.noreply.github.com>
2020-01-12 10:43:06 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
-ways = ["You have nothing to eat!",
-        "Your joke is less funny than your father's!",
-        "BOOOOOOOOOOOOOOOOOM",
-        "You died because you are too poor.",
-        "Your Mum kill you because you haven't do your homework yet."
-        "You died of fear."
-        "You died because you didn't do your homework."
-        "You're dead. There's no reason"
-        "Virus invasion!"
-        "Reject a face-saving offer."
-        "Get ready for your death!"]
+#!\Users\ccboy\Python\DarkRoom-I-master\Rooms.py
+from random import randint 
+
+def printdata():
+    data = ["You have nothing to eat!",
+            "Your joke is less funny than your father's!",
+            "BOOOOOOOOOOOOOOOOOM",
+            "You died because you are too poor.",
+            "Your Mum kill you because you haven't do your homework yet.",
+            "You died of fear.",
+            "You died because you didn't do your homework.",
+            "You're dead. There's no reason",
+            "Virus invasion!",
+            "Reject a face-saving offer.",
+            "Get ready for your death!"]
+    print(data[int(randint(0,10))])
